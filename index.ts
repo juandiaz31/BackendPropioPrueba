045335bf09368b87d5414e25e7daf16e7927c64b
Juan <jsdiazg1@gmail.com>
2021-11-09 23:18:59 -0500

    correcion de inicializacion

--- index.ts
+++ index.ts
@@ -1,15 +1,9 @@
-import { connect } from "mongoose";
-
-const conectarBD = async () => {
-    return await connect(
-      'mongodb+srv://juandiaz31:juandiaz31@cluster-prueba.v3rv6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-    )
-      .then(() => {
-        console.log('Conexion exitosa');
-      })
-      .catch((e) => {
-        console.error('Error conectando a la bd', e);
-      });
-  };
+import conectarBD from "./db/db";
   
-  export default conectarBD;
+const main = async () => {
+  await conectarBD();
+
+  
+};
+
+main();
