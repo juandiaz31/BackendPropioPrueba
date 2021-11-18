17435d125dfc7065698cdd12b92f81210484d53b
Sebastian Diaz <jsdiazg1@gmail.com>
2021-11-18 10:03:38 -0500

    corregir querys de avances

--- models/avance/resolvers.ts
+++ models/avance/resolvers.ts
@@ -3,7 +3,9 @@ import { AdvancementModel } from "./avance";
 const resolverAvance = {
   Query: {
     Avances: async (parent, args) => {
-      const avances = await AdvancementModel.find();
+      const avances = await AdvancementModel.find()
+        .populate("proyecto")
+        .populate("creadoPor");
       return avances;
     },
   },
@@ -11,7 +13,7 @@ const resolverAvance = {
   Mutation: {
     crearAvance: async (parent, args) => {
       const avanceCreado = await AdvancementModel.create({
-        fecha: args.fecha,
+        fecha: new Date(args.fecha),
         descripcion: args.descripcion,
         proyecto: args.proyecto,
         creadoPor: args.creadoPor,
